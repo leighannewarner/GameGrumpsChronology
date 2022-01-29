8855a6f1e1e5a6432875048c181c6a352c1d8976
leighannewarner <leighannewarner@gmail.com>
2022-01-28 22:58:27 -0800

    Update process tests

--- playlist_process_uploads_test.py
+++ playlist_process_uploads_test.py
@@ -8,19 +8,18 @@ import json
 from unittest.mock import patch
 
 
-@patch('database_reads.get_video_row', return_value=['abc123', None, 'ghi789', 'abc123', 'def456', None])
+@patch('database_reads.get_video_row')
 @patch('database_mutations.insert_videos')
 @patch('youtube_reads.list_videos_in_playlist')
 @patch('playlists_utils.authorize')
 @patch('playlists_utils.dry_run_prompt')
 class PlaylistOperationsTest(unittest.TestCase):
-
     NUM_UPLOADS_PLAYLISTS = len(playlist_config.UPLOADS_PLAYLISTS)
 
     def test_process_uploads_filters_processed(self, dry_run_prompt, authorize, list_videos_in_playlist, insert_videos,
                                                get_video_row):
-
         utils.dry_run = False
+        get_video_row.return_value = ['abc123', None, 'ghi789', 'abc123', 'def456', None]
         video_response = [
             json.loads(
                 '''{"snippet": {"resourceId": {"videoId": "abc123"}},
@@ -45,15 +44,16 @@ class PlaylistOperationsTest(unittest.TestCase):
     def test_process_uploads_dry_run(self, dry_run_prompt, authorize, list_videos_in_playlist, insert_videos,
                                      get_video_row):
         utils.dry_run = True
+        get_video_row.return_value = None
         video_response = [
             json.loads(
-                '''{"snippet": {"resourceId": {"videoId": "abc123"}}, 
+                '''{"snippet": {"resourceId": {"videoId": "abc123"}},
                 "contentDetails": {"videoPublishedAt": "2012-06-20T22:45:24.000Z"}}'''),
             json.loads(
-                '''{"snippet": {"resourceId": {"videoId": "def456"}}, 
+                '''{"snippet": {"resourceId": {"videoId": "def456"}},
                 "contentDetails": {"videoPublishedAt": "2012-07-20T22:45:24.000Z"}}'''),
             json.loads(
-                '''{"snippet": {"resourceId": {"videoId": "ghi789"}}, 
+                '''{"snippet": {"resourceId": {"videoId": "ghi789"}},
                 "contentDetails": {"videoPublishedAt": "2012-08-20T22:45:24.000Z"}}'''),
         ]
         list_videos_in_playlist.return_value = video_response
@@ -65,6 +65,24 @@ class PlaylistOperationsTest(unittest.TestCase):
         authorize.assert_called()
         self.assertEqual(get_video_row.call_count, 3 * self.NUM_UPLOADS_PLAYLISTS)
         insert_videos.assert_not_called()
+
+    def test_process_uploads_inserts_video(self, dry_run_prompt, authorize, list_videos_in_playlist, insert_videos,
+                                           get_video_row):
+        utils.dry_run = False
+        get_video_row.return_value = None
+        video_response = [
+            json.loads(
+                '''{"snippet": {"resourceId": {"videoId": "abc123"}},
+                "contentDetails": {"videoPublishedAt": "2012-06-20T22:45:24.000Z"}}'''),
+        ]
+        list_videos_in_playlist.return_value = video_response
+
+        process_uploads.process()
+
+        insert_videos.assert_called_with([{'video_id': 'abc123', 'date': '2012-06-20T22:45:24.000Z',
+                                           'playlist_order': '2012-06-20T22:45:24.000Z~0000'},
+                                          {'video_id': 'abc123', 'date': '2012-06-20T22:45:24.000Z',
+                                           'playlist_order': '2012-06-20T22:45:24.000Z~0000'}])
 
 
 if __name__ == '__main__':
