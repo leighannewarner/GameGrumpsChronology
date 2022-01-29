de996453bee296d8320a47814f5589253dc5ce8e
leighannewarner <leighannewarner@gmail.com>
2022-01-29 00:03:39 -0800

    Add function to list all playlists on a given youtube channel

--- youtube_reads.py
+++ youtube_reads.py
@@ -23,6 +23,7 @@ def list_videos_in_playlist(youtube_client, playlist_id):
         if not pagination_token:
             break
 
+    print('\n')
     return videos
 
 
@@ -44,3 +45,47 @@ def _list_videos_in_playlist_internal(youtube_client, playlist_id, pagination_to
     )
     response = utils.execute_request(request)
     return response
+
+
+def list_playlists_on_channel(youtube_client, channel_id):
+    """
+    List playlists on a given Youtube channel.
+
+    :param youtube_client:
+    :param channel_id:
+    :return:
+    """
+
+    pagination_token = None
+    playlists = []
+
+    while True:
+        print('.', end='', sep='')
+
+        response = _list_playlists_on_channel_internal(youtube_client, channel_id, pagination_token)
+        playlists.extend(response.get('items'))
+        pagination_token = response.get('nextPageToken')
+        if not pagination_token:
+            break
+
+    print('\n')
+    return playlists
+
+
+def _list_playlists_on_channel_internal(youtube_client, channel_id, pagination_token):
+    """
+    Perform list request to the YouTube API
+
+    :param youtube_client:
+    :param channel_id:
+    :param pagination_token:
+    :return:
+    """
+
+    request = youtube_client.playlists().list(
+        part=['id', 'snippet'],
+        maxResults=50,
+        channelId=channel_id,
+        pageToken=pagination_token
+    )
+    return utils.execute_request(request)
