c85d56c41cdf2e3d5191d1873a5d323a239acdb6
leighannewarner <leighannewarner@gmail.com>
2022-03-12 19:25:18 -0800

    Add more read utils to database

--- database_reads.py
+++ database_reads.py
@@ -1,3 +1,4 @@
+import database_utils
 import database_utils as utils
 
 
@@ -26,14 +27,34 @@ def get_video_queue():
     """
     Retrieves a list of videos that need to be processed and them as maps of database values.
 
-    :return:
+    :return: A list of video objects
     """
 
     videos = []
     for row in utils.execute(
-            '''SELECT id,created_playlist_id,playlist_order,processed FROM existing_videos WHERE processed = ? 
-            AND created_playlist_id IS NOT NULL ORDER BY playlist_order DESC''',
+            '''SELECT id,created_playlist_id,playlist_order,processed FROM existing_videos 
+            WHERE created_playlist_id IS NULL 
+            ORDER BY playlist_order DESC''',
             {'processed': False}):
+        videos.append(
+            {'video_id': row[0], 'playlist_id': row[1], 'order': row[2], 'processed': row[3]})
+    return videos
+
+
+def get_video_queue_for_range(start_date, end_date):
+    """
+    Retrieves a list of videos that need to be processed within a date range and them as maps of database values.
+
+    :return: A list of video objects
+    """
+
+    videos = []
+    for row in utils.execute(
+            '''SELECT id,created_playlist_id,playlist_order,processed FROM existing_videos 
+            WHERE playlist_order >= :start_date AND playlist_order <= :end_date
+            ORDER BY playlist_order DESC''',
+            {'start_date': database_utils.get_order_string(start_date, 0),
+             'end_date': database_utils.get_order_string(end_date, 9999)}):
         videos.append(
             {'video_id': row[0], 'playlist_id': row[1], 'order': row[2], 'processed': row[3]})
     return videos
@@ -68,3 +89,16 @@ def get_created_playlist_id(start_date, end_date):
         {'start_date': start_date, 'end_date': end_date})
 
     return row[0] if row else None
+
+
+def get_all_created_playlists():
+    """
+    Retrieves all the created playlists.
+
+    :return: A list of playlist objects
+    """
+
+    playlists = []
+    for row in utils.execute('''SELECT id,start_date,end_date FROM created_playlists ORDER BY start_date DESC'''):
+        playlists.append({'playlist_id': row[0], 'start_date': row[1], 'end_date': row[2]})
+    return playlists
